9e9924bd0d8a96c0178cc1806716caed34e5b149
JeffSabol <jeffreysabol345@gmail.com>
2024-09-02 20:42:16 -0400

    log player id when they leave so clients can know to remove it

--- Scenes/Multiplayer/multiplayer_test.gd
+++ Scenes/Multiplayer/multiplayer_test.gd
@@ -11,6 +11,7 @@ var peer = ENetMultiplayerPeer.new()
 		#peer.create_server(35000)
 		#multiplayer.multiplayer_peer = peer
 		#multiplayer.peer_connected.connect(_add_player)
+		#multiplayer.peer_disconnected.connect(_on_player_disconnected)
 		#_add_player()
 	
 func _add_player(id = 1):
@@ -23,4 +24,5 @@ func _on_join_pressed():
 	multiplayer.multiplayer_peer = peer
 
 func _on_player_disconnected(id):
-	print("Player id left: " + id)
+	print("Player id left: " + str(id))
+	
