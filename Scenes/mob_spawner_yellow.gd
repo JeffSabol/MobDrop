54eb5d2d8be6e5d3ad9d4d5b33a45c0ab2fdf623
Jeff <jeffreysabol345@gmail.com>
2024-12-29 18:46:27 -0500

    Added mob spawners and enemies for each color

--- Scenes/mob_spawner_yellow.gd
+++ Scenes/mob_spawner_yellow.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-
-@export var monster: PackedScene  = preload("res://Scenes/Enemies/eyeball_blue.tscn")
+@export var monster: PackedScene  = preload("res://Scenes/Enemies/eyeball_yellow.tscn")
 @export var spawn_position: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +12,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_timer_timeout() -> void:
-	print("spawn blue monster")
+	print("spawn yellow monster")
 	if monster:
 		var mob = monster.instantiate()
 		mob.position = spawn_position
