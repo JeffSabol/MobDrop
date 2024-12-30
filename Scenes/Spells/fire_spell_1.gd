5bb4a5e4e02b03ae81ba0617c3e88d3502206818
Jeff <jeffreysabol345@gmail.com>
2024-12-29 19:07:10 -0500

    Spells react to only the correct monster now

---
+++ Scenes/Spells/fire_spell_1.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+
+var damage = 10
+
+func _ready():
+	play("default")  # Play the initial animation
+	$Timer.start()  # Start a timer if you want to auto-destroy
+
+func _on_Area2D_body_entered(body):
+	if body.name == "EyeballRed":  # Assuming enemies are in this group
+		body.get_parent().take_damage(damage)
+
+func _on_timer_timeout() -> void:
+	queue_free()  # Destroy the spell after the animation ends
