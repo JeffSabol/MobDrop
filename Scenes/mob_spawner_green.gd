c911545c2a97154b72c9d3f80f856b35c0c3315b
Jeff <jeffreysabol345@gmail.com>
2024-12-29 19:37:15 -0500

    Added a cap of 10 monsters per spawn to stop intense lag

--- Scenes/mob_spawner_green.gd
+++ Scenes/mob_spawner_green.gd
@@ -1,21 +1,18 @@
 extends Node2D
 
-@export var monster: PackedScene  = preload("res://Scenes/Enemies/eyeball_green.tscn")
+@export var monster: PackedScene = preload("res://Scenes/Enemies/eyeball_green.tscn")
 @export var spawn_position: Vector2 = Vector2.ZERO
+@export var max_spawn_count: int = 10
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var spawn_count: int = 0
 
 func _on_timer_timeout() -> void:
-	print("spawn green monster")
-	if monster:
-		var mob = monster.instantiate()
-		mob.position = spawn_position
-		add_child(mob)
+	# Check if we've reached the spawn limit
+	if spawn_count < max_spawn_count:
+		if monster:
+			var mob = monster.instantiate()
+			mob.position = spawn_position
+			add_child(mob)
+			spawn_count += 1
 	else:
-		null
+		$Timer.stop()
